3a60ae4617d5735b2531f2c41dc48ed072ee50ea
Michael Girgis <knaffles@gmail.com>
2012-03-09 12:56:10 -0600

    Added pages, illustration and icons section, and more js for home carousel

--- ee/js/main.js
+++ ee/js/main.js
@@ -3,6 +3,16 @@ $('#home-carousel').carousel({
   interval: 5000
 })
 
+$('#home-carousel').bind('slid', function() {
+	// Remove any active classes on thumbs
+	$('#home-carousel-thumbs .item').removeClass('active');
+	// Get the index of the currently active carousel item
+	var activeSlide = $('.carousel-inner .active').index();
+	console.log(activeSlide);
+	$('#home-carousel-thumbs .item').eq(activeSlide).addClass('active');
+
+})
+
 // Enable carousel buttons
 $('#home-carousel-thumbs .item').click(function() {
 	console.log($(this).index());
